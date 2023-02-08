337544ad6a90380ed38099c9601e8301abe933d4
shalamike <m.shalaby1994@gmail.com>
2023-02-08 15:43:33 +0000

    created empty methods to implement, now to create test cases

--- Palindrome/src/main/java/org/example/Pallindrome.java
+++ Palindrome/src/main/java/org/example/Pallindrome.java
@@ -1,8 +1,36 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Pallindrome {
 
-    public Boolean isAPalindrome(String Word){
+    //Method 1
+    public String sequenceOfPalindromes(String sentence){
         return null;
+    }
+
+    public String[] createArrayOfPalindromes(String palindromeSequence){
+        return null;
+    }
+
+    public String longestPalindrome(String[] palindromeArray){
+        return null;
+    }
+
+    //method 2 using arrayList to eliminate the need to create a list of palindromes
+
+    public Boolean isAPalindrome(String word){
+        String reversedWord = wordToReverse(word);
+        if (word.equals(reversedWord))
+            return true;
+        else
+            return false;
+    }
+
+    public String wordToReverse(String word){
+        String reversedWord = "";
+        for(int i = word.length() -1; i>= 0; i--)
+            reversedWord += word.charAt(i);
+        return reversedWord;
     }
 }
